8e041b1986df25e61660f1af7a87248118b3bfc6
natesholland <natesholland@gmail.com>
2013-09-26 22:23:45 -0700

    initializing the rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FightImpunityHaiti::Application.config.secret_token = 'a9a6bed17692d84a7bf169aae72250703888a4a9ba512971e4254a4424728490afb06543592196eb456ba652b0d2635c1c66fde448ea62eaac89329d03b57b75'
