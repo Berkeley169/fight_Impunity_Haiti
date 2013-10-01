f590c8b679acb8200bfc932a09eac50aa3d46c64
natesholland <natesholland@gmail.com>
2013-10-01 15:19:17 -0700

    adds a pages controller for the basic pages and removes the public index.html file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FightImpunityHaiti::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
   resources :documents
 
 
@@ -51,7 +57,7 @@ FightImpunityHaiti::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
