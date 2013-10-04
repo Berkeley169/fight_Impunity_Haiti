45c05c5ad773c50a014749d3ab178d45bedd5714
natesholland <natesholland@gmail.com>
2013-10-04 14:36:18 -0700

    adding a datetime and something trivial to the pictures model . #4

--- db/migrate/20131004205722_create_pictures.rb
+++ db/migrate/20131004205722_create_pictures.rb
@@ -1,8 +1,8 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
-
-      t.timestamps
+    	t.datetime :date
+      	t.timestamps
     end
   end
 end
