5ed224f61eab32b061ac4019aeec4535911a0544
Nicholas Herson <nicholas.herson@berkeley.edu>
2013-10-10 13:37:49 -0700

    further improvements to Picture model and its specs

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -9,19 +9,16 @@ class Picture < ActiveRecord::Base
     validates_attachment :pic, :presence => true,
                           :content_type => {:content_type => ['image/jpg', 'image/png', 'image/jpeg']}
 
-  	def get_translations
-  		#needs to be implemented
-        PictureLang.where(:picture_id => self.id)
+  def get_translations
+    self.picture_langs
 	end
 
 	def get_language(lang)
-		#needs to be implemented
-        #PictureLang.where(:picture_id => self.id, :lang => lang).first
-        if self.picture_langs.where(:lang => lang)
-        	self.picture_langs.where(:lang => lang).first
-        else
-        	return nil
-        end
+    if self.picture_langs.where(:lang => lang)
+    	self.picture_langs.where(:lang => lang).first
+    else
+      return nil
+    end
 	end
 
 end
