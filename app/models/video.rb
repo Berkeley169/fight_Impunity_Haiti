e400cf5f2180998fc618cb17885fcc70bb601155
pklazer <skylrroj0714@aol.com>
2013-10-12 09:02:07 -0700

    added video to model and factory girl specs

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,12 +1,22 @@
 class Video < ActiveRecord::Base
-  # attr_accessible :title, :body
-  has_many :video_lang
+  attr_accessible :name, :vid
+  has_many :video_langs
   belongs_to :item
-    def get_translations
-  		#needs to be implemented
+  accepts_nested_attributes_for :video_langs
+  belongs_to :item
+  validates :name,:vid, presence: true
+  
+  def get_translations
+    self.video_langs
 	end
 
 	def get_language(lang)
-		#needs to be implemented
+    if self.video_langs.where(:lang => lang)
+      self.video_langs.where(:lang => lang).first
+    else
+      return nil
+    end
 	end
+
+
 end
