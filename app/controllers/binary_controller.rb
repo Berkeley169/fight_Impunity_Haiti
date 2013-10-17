3518318ef2127e57e34fe7e44572a61eaf1c76e9
natesholland <natesholland@gmail.com>
2013-10-16 19:54:34 -0700

    scaffolding a lot of the controllers and views so we can make forms for the models

---
+++ app/controllers/binary_controller.rb
@@ -0,0 +1,10 @@
+class BinaryController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/video_controller.rb
@@ -0,0 +1,10 @@
+class VideoController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
