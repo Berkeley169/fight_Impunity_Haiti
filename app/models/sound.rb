97c98e4f7981c71e7ce802d9889c4dee9e35a33a
Nicholas Herson <nicholas.herson@berkeley.edu>
2013-10-19 16:01:48 -0700

    Initial sound model support

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -2,16 +2,34 @@
 # this should not have an attachment but each specific
 # language should have one
 class Sound < ActiveRecord::Base
+<<<<<<< HEAD
+  # attr_accessible :title, :body
+  attr_accessible :sound, :name, :date, :sound_langs_attributes
+  has_many :sound_langs
+  accepts_nested_attributes_for :sound_langs
+  has_attached_file :snd
+  belongs_to :item
+  validates :name, :presence => true
+  validates_attachment :snd, :presence => true,
+            :content_type => {:content_type => ['audio/mpeg3', 'audio/mpeg']}
+=======
   attr_accessible :name, :snd
   has_many :sound_langs
   belongs_to :item
   has_and_belongs_to_many :tags
+>>>>>>> 1f16ff1949748d67ac88db61ca28e4b96bedb664
 
 	def get_translations
 			#needs to be implemented
+      self.sound_langs
 	end
 
 	def get_language(lang)
 		#needs to be implemented
+    if self.sound_langs.where(:lang => lang)
+      self.sound_langs.where(:lang => lang).first
+    else
+      nil
+    end
 	end
 end
