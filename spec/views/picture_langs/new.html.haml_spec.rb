873e9f065297bfa20c8a92b6cdae1ea7aed3132f
natesholland <natesholland@gmail.com>
2013-10-23 17:14:23 -0700

    removing a lot of the pending tests and deleting some trivial tests

--- spec/views/picture_langs/new.html.haml_spec.rb
+++ spec/views/picture_langs/new.html.haml_spec.rb
@@ -6,10 +6,5 @@ describe "picture_langs/new" do
   end
 
   it "renders new picture_lang form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", picture_langs_path, "post" do
-    end
   end
 end
