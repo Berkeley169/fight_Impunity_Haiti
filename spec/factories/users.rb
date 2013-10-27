289398a499d1d95bc5524c949851e2d7f20432f7
Colin Murphy <themurph23@yahoo.com>
2013-10-26 21:31:27 -0700

    fixed merge conflict on users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,28 +8,18 @@ FactoryGirl.define do
     "email#{n}@gmail.com"
   end
   factory :editor, :class => 'user' do |e|
-<<<<<<< HEAD
     e.first_name "Eddy"
     e.last_name "Tor"
     e.email "editor@gmail.com"
-=======
-    e.email
-    e.name
->>>>>>> 8c1761e395ee645b65e0192aeb2541135cf84c4f
     e.password "abcd1234"
     e.password_confirmation { |u| u.password }
     e.role "Editor"
     e.lang "English"
   end
   factory :manager, :class => 'user' do |m|
-<<<<<<< HEAD
     m.first_name "M. Anna"
     m.last_name "Ger"
     m.email "manager@gmail.com"
-=======
-    m.email
-    m.name
->>>>>>> 8c1761e395ee645b65e0192aeb2541135cf84c4f
     m.password "abcd1234"
     m.password_confirmation { |u| u.password }
     m.role "Manager"
