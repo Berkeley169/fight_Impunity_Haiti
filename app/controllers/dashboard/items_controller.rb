7d140fc19427780d354faf578edd63d2b60e704b
natesholland <natesholland@gmail.com>
2013-11-06 18:43:20 -0800

    making it so the dashboard will show all of the documents #39

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -3,6 +3,7 @@ class Dashboard::ItemsController < DashboardController
   def index
     setup_dashboard
     @title = 'All Documents'
+    @Documents = Picture.all + Text.all + Sound.all + Video.all + Binary.all
   end
 
    	
