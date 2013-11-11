02f46d86fc8e5f3f5b932b50330ddce7da5d0e0b
pklazer <skylrroj0714@aol.com>
2013-11-10 20:55:51 -0800

    fixed the presentation of file names in view for uploaded text files

--- app/models/text_lang.rb
+++ app/models/text_lang.rb
@@ -2,7 +2,7 @@
 # each language will have an attachment for the specific language
 class TextLang < ActiveRecord::Base
   attr_accessible :description, :lang, :title, :txt, :plain_text, :published, :status
-  has_attached_file :txt
+  has_attached_file :txt, :styles => {:small => "150x150"}
   belongs_to :text
   validates :lang, presence: true
 end
