bf623a2c53569950a3f59893892b7bca73f25d9b
natesholland <natesholland@gmail.com>
2013-11-13 21:50:27 -0800

    adding some helper methods so we can take the current locale and turn it into a string to be used for metaprogramming. Nick you'll find this helpful and like it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+	def get_locale
+		I18n.locale
+	end
+
+	def locale_to_word(locale = I18n.locale)
+		if locale == :en
+			:English
+		elsif locale == :fr
+			:French
+		elsif locale == :es
+			:Spanish
+		elsif locale == :ht
+			:Creole
+		else
+			:English
+		end	
+	end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+	describe "get_locale" do
+		it "should return the default locale if none is set" do
+			I18n.locale = :en
+			get_locale.should == :en
+		end
+		it "should return the correct locale when it is not the default" do
+			I18n.locale = :fr
+			get_locale.should == :fr
+			I18n.locale = :es
+			get_locale.should == :es
+			I18n.locale = :ht
+			get_locale.should == :ht
+		end
+	end
+
+	describe "locale_to_word" do
+		it "should return the correct word based on the locale" do
+			I18n.locale = :en
+			locale_to_word.should == :English
+			I18n.locale = :fr
+			locale_to_word.should == :French
+			I18n.locale = :es
+			locale_to_word.should == :Spanish
+			I18n.locale = :ht
+			locale_to_word.should == :Creole
+		end
+		it "should return the correct word based on the locale when a locale is passed in" do
+			locale_to_word(:en).should == :English
+			locale_to_word(:fr).should == :French
+			locale_to_word(:es).should == :Spanish
+			locale_to_word(:ht).should == :Creole
+		end
+
+		it "should work when the else statement happens and send you to English" do
+			locale_to_word(:foobar).should == :English
+		end
+	end
+end
