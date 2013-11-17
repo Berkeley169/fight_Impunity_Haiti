0bbdaaceb26d6244ded624ac9bfd97de9c0cd5be
Nicholas Herson <nicholas.herson@berkeley.edu>
2013-11-17 01:57:52 -0800

    fixing bugs caused in spec tests due to permission changes

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -19,23 +19,23 @@ describe ApplicationHelper do
 	describe "locale_to_word" do
 		it "should return the correct word based on the locale" do
 			I18n.locale = :en
-			locale_to_word.should == :English
+			locale_to_word.should == :english
 			I18n.locale = :fr
-			locale_to_word.should == :French
+			locale_to_word.should == :french
 			I18n.locale = :es
-			locale_to_word.should == :Spanish
+			locale_to_word.should == :spanish
 			I18n.locale = :ht
-			locale_to_word.should == :Creole
+			locale_to_word.should == :creole
 		end
 		it "should return the correct word based on the locale when a locale is passed in" do
-			locale_to_word(:en).should == :English
-			locale_to_word(:fr).should == :French
-			locale_to_word(:es).should == :Spanish
-			locale_to_word(:ht).should == :Creole
+			locale_to_word(:en).should == :english
+			locale_to_word(:fr).should == :french
+			locale_to_word(:es).should == :spanish
+			locale_to_word(:ht).should == :creole
 		end
 
 		it "should work when the else statement happens and send you to English" do
-			locale_to_word(:foobar).should == :English
+			locale_to_word(:foobar).should == :english
 		end
 	end
 end
