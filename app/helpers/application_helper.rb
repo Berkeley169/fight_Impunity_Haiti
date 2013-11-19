83ead1603fad27f30cd2e1486048ab546a4a3ae5
Greg Dehmlow <gdehmlow@gmail.com>
2013-11-19 01:06:24 -0800

    Broke everything; rebuilding, starting with navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,47 +1,57 @@
 module ApplicationHelper
-	def get_locale
-		I18n.locale
-	end
+  def get_locale
+    I18n.locale
+  end
 
-	def locale_to_word(locale = I18n.locale)
-		if locale == :en
-			:English
-		elsif locale == :fr
-			:French
-		elsif locale == :es
-			:Spanish
-		elsif locale == :ht
-			:Creole
-		else
-			:English
-		end	
-	end
+  def locale_to_word(locale = I18n.locale)
+    if locale == :en
+      :English
+    elsif locale == :fr
+      :French
+    elsif locale == :es
+      :Spanish
+    elsif locale == :ht
+      :Creole
+    else
+      :English
+    end 
+  end
 
-	# TODO: change these helpers to be localized
-	def pretty_date(date)
-		if date.today?
-			return "today"
-		elsif date.to_date == Date.yesterday
-			return "yesterday"
-		else
-			return date.strftime("%b %d, %Y")
-		end
-	end
+  # TODO: change these helpers to be localized
+  def pretty_date(date)
+    if date.today?
+      return t(:today)
+    elsif date.to_date == Date.yesterday
+      return t(:yesterday)
+    else
+      return date.strftime("%b %d, %Y")
+    end
+  end
 
-	def doc_description(document)
-		document.send((document.class.to_s.downcase + "_langs").to_sym).each do |dl|
-			if !dl.description.nil?
-				return dl.description
-			end
-		end
-		return "No description available"
-	end
-	
-	def doc_title(document)
-		document.send((document.class.to_s.downcase + "_langs").to_sym).each do |dl|
-			if !dl.title.nil?
-				return dl.title
-			end
-		end
-	end
+  def doc_description(document)
+    document.send((document.class.to_s.downcase + "_langs").to_sym).each do |dl|
+      if !dl.description.nil?
+        return dl.description
+      end
+    end
+    return "No description available"
+  end
+  
+  def doc_title(document)
+    document.send((document.class.to_s.downcase + "_langs").to_sym).each do |dl|
+      if !dl.title.nil?
+        return dl.title
+      end
+    end
+  end 
+
+  def current_locale? locale, element
+    get_locale == element.to_sym || locale == element
+  end
+
+  def current_locale_class(locale, element)
+    return 'active' if get_locale.nil? && locale.nil? && element == 'en' || 
+                       get_locale == element.to_sym ||
+                       locale == element
+  end
 end
