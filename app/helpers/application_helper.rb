6fb62168fb1c52f891b9b9f2827d1cfc2bf8b3c6
Nicholas Herson <nicholas.herson@berkeley.edu>
2013-11-19 18:33:16 -0800

    edit permissions nearly done
    Just need to polish views and make sure cukes pass

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,43 @@ module ApplicationHelper
   def auth_to_view(document_lang)
     user_signed_in? or document_lang.status == 'published'
   end
+
+  def auth_to_edit_status(document_lang)
+    if not user_signed_in?
+      return false
+    end
+    if current_user.role == "Manager"
+      return true
+    elsif current_user.role == "Editor" and ["in_progress", "new"].include?(document_lang.status)
+      return true
+    else
+      return false
+    end
+  end
+
+  def auth_to_edit_lang(document_lang)
+    if not user_signed_in?
+      return false
+    end
+    if current_user.role == "Manager"
+      return true
+    elsif current_user.role == "Editor" and document_lang.status == "in_progress"
+      return true
+    else
+      return false
+    end
+  end
+
+  def auth_to_edit_meta(document)
+    if not user_signed_in?
+      return false
+    end
+    if current_user.role == "Manager"
+      return true
+    elsif current_user.role == "Editor" and not (document.published or document.pending)
+      return true
+    else
+      return false
+    end
+  end
 end
