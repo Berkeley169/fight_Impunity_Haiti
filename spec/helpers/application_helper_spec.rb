c30c1eb3943b823148b429940098000f7ed880e7
Ryan Killory <ryankillory@gmail.com>
2013-11-23 17:28:55 -0800

    added some spec tests to application_helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+	before(:each) do
+		@a = FactoryGirl.create(:text)
+	end
 	describe "get_locale" do
 		it "should return the default locale if none is set" do
 			I18n.locale = :en
@@ -38,4 +41,47 @@ describe ApplicationHelper do
 			locale_to_word(:foobar).should == :english
 		end
 	end
+
+	describe "current_locale?" do
+		it "should work" do
+			I18n.locale = :en
+			current_locale?(get_locale, :en).should be_true
+		end
+	end
+
+	describe "current_locale_class" do
+		it "should work" do
+			I18n.locale = :en
+			current_locale_class(get_locale, :en).should == "active"
+		end
+	end
+
+	#describe "pretty_date" do
+	#	it "should work for today" do
+	#		I18n.locale = :en
+	#		pretty_date(:today).should be(t(:today))
+	#	end
+	#end
+
+	describe "doc_description" do
+		it "should work if there is no description" do
+			doc_description(@a).should == "No description available"
+		end
+		it "should work if there is a description" do
+			@a.send((@a.class.to_s.downcase + "_langs").to_sym).each do |doc|
+				doc.description = "ay"
+			end
+			doc_description(@a).should == "ay"
+		end
+	end
+
+	describe "doc_title" do
+		it "should return the title" do
+			@a.send((@a.class.to_s.downcase + "_langs").to_sym).each do |doc|
+				doc.title = "woo"
+			end
+			doc_title(@a).should == "woo"
+		end
+	end
 end
+
