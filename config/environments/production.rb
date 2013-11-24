409e17db4df83b768cf8a62acd5cedba9e454bcd
Nicholas Herson <nicholas.herson@berkeley.edu>
2013-11-24 13:43:18 -0800

    changing the way we precompile assets to fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ FightImpunityHaiti::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['dashboard.css']
+  config.assets.precompile += ['screen.css','doc_index.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
